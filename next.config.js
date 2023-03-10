8a55fee06a552f25c9dcb48c0367ee86d8d7866a
jihwa <mungsome@gmail.com>
2023-03-10 16:39:03 +0900

    img domain nextConfig 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ["https://i.pinimg.com/"],
+  },
 };
 
 module.exports = nextConfig;
