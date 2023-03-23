cc82b3252aac2b51fc18c771a9e599b428ffb4e4
jihwa <mungsome@gmail.com>
2023-03-23 14:10:22 +0900

    h1 -> span

--- components/nav.js
+++ components/nav.js
@@ -4,11 +4,13 @@ import DarkModeToggleButton from "./DarkModeToggleButton";
 export default function Nav() {
   return (
     <>
-      <div className="p-6 md:pr-10 w-full sm:w-2/5  sm:fixed sm:top-0 sm:h-screen md:w-2/5 lg:w-1/5">
-        <Link href="/">
-          <h1>Git Log ─ Portfolio</h1>
-        </Link>
-        <p className="pt-6 pb-8">
+      <div className="p-6 md:pr-10 w-full sm:w-2/5  sm:fixed sm:top-0 sm:h-screen md:w-2/5 lg:w-1/5 dark:text-white">
+        <h1>
+          <Link href="/">
+            <span>Git Log ─ Portfolio</span>
+          </Link>
+        </h1>
+        <p className="pt-6 pb-8 dark:text-white">
           hello, I am looking for a job. I used to work in editorial design. I
           like to make new things. Also I like to look around the new website.
           In my free time, I read books or watch LCK.
@@ -33,7 +35,7 @@ export default function Nav() {
         <a
           href="https://github.com/yunjoa/"
           target="_blank"
-          className="pb-2 block hover:underline underline-offset-4"
+          className="pb-2 block hover:underline underline-offset-4 dark:text-white"
         >
           git
         </a>
